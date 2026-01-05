54c11ba83c2fab48910491c2bd3db5fedb32780d
cjfwil <23485052+cjfwil@users.noreply.github.com>
2026-01-05 05:19:06 +0000

    some cleanup

--- main.cpp
+++ main.cpp
@@ -36,11 +36,22 @@ static struct
 static struct
 {
     // todo drop d3d12 state stuff in here
-} d3d12State;
+    // init
+    IDXGIFactory6 *factory = nullptr;
+    IDXGIAdapter4 *hardwareAdapter = nullptr;
+    ID3D12Device *device = nullptr;
+    IDXGISwapChain1 *swapChain1 = nullptr; // only for initialisation
+    IDXGISwapChain4 *swapChain = nullptr;
+    ID3D12CommandQueue *commandQueue = nullptr;
+    ID3D12DescriptorHeap *rtvHeap = nullptr;
+    ID3D12DescriptorHeap *srvHeap = nullptr;
+    static const int frameCount = 2;
+    ID3D12CommandAllocator *commandAllocators[frameCount] = {};
+    // end of init
+} renderState;
 
 int main(void)
 {
-
     if (!SetExtendedMetadata())
         return 1;
 
@@ -80,25 +91,21 @@ int main(void)
 
     HRESULT hr = S_OK;
 
-    IDXGIFactory6 *factory = nullptr;
-    hr = CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory));
+    hr = CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&renderState.factory));
     if (FAILED(hr))
     {
         errhr("CreateDXGIFactory2 failed", hr);
         return 1;
     }
 
-    IDXGIAdapter4 *hardwareAdapter = nullptr;
-
-    hr = factory->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&hardwareAdapter));
+    hr = renderState.factory->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&renderState.hardwareAdapter));
     if (FAILED(hr))
     {
         errhr("EnumAdapterByGpuPreference failed", hr);
         return 1;
     }
 
-    ID3D12Device *device = nullptr;
-    hr = D3D12CreateDevice(hardwareAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
+    hr = D3D12CreateDevice(renderState.hardwareAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&renderState.device));
     if (FAILED(hr))
     {
         errhr("D3D12CreateDevice failed", hr);
@@ -112,15 +119,14 @@ int main(void)
     queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
     queueDesc.NodeMask = 0;
 
-    ID3D12CommandQueue *commandQueue = nullptr;
-    hr = device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&commandQueue));
+    hr = renderState.device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&renderState.commandQueue));
     if (FAILED(hr))
     {
         errhr("CreateCommandQueue failed", hr);
         return 1;
     }
 
-    static const int frameCount = 2;
+    
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
     swapChainDesc.Width = (UINT)width;
     swapChainDesc.Height = (UINT)height;
@@ -129,7 +135,7 @@ int main(void)
     swapChainDesc.SampleDesc.Count = 1;
     swapChainDesc.SampleDesc.Quality = 0;
     swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    swapChainDesc.BufferCount = (UINT)frameCount;
+    swapChainDesc.BufferCount = (UINT)renderState.frameCount;
     swapChainDesc.Scaling = DXGI_SCALING_NONE;
     swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
     swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
@@ -144,71 +150,68 @@ int main(void)
         return 1;
     }
 
-    IDXGISwapChain1 *swapChain1 = nullptr;
-    hr = factory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, &swapChain1);
+    hr = renderState.factory->CreateSwapChainForHwnd(renderState.commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, &renderState.swapChain1);
     if (FAILED(hr))
     {
         errhr("CreateSwapChainForHwnd failed", hr);
         return 1;
     }
 
-    IDXGISwapChain4 *swapChain = nullptr;
-    hr = swapChain1->QueryInterface(IID_PPV_ARGS(&swapChain));
+    hr = renderState.swapChain1->QueryInterface(IID_PPV_ARGS(&renderState.swapChain));
     if (FAILED(hr))
     {
         errhr("QueryInterface on swapChain1 failed", hr);
         return 1;
     }
-    swapChain1->Release();
-    UINT frameIndex = swapChain->GetCurrentBackBufferIndex();
+    renderState.swapChain1->Release();
+    UINT frameIndex = renderState.swapChain->GetCurrentBackBufferIndex();
 
     // create descriptor heaps
     D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
     rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-    rtvHeapDesc.NumDescriptors = (UINT)frameCount;
+    rtvHeapDesc.NumDescriptors = (UINT)renderState.frameCount;
     rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
     rtvHeapDesc.NodeMask = 0;
-    ID3D12DescriptorHeap *rtvHeap = nullptr;
-    hr = device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
+
+    hr = renderState.device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&renderState.rtvHeap));
     if (FAILED(hr))
     {
         errhr("CreateDescriptorHeap failed (rtvHeap)", hr);
         return 1;
     }
-
-    ID3D12DescriptorHeap *srvHeap = nullptr;
+    
     D3D12_DESCRIPTOR_HEAP_DESC srvHeapDesc = {};
     srvHeapDesc.NumDescriptors = 2; // CBV + SRV
     srvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
     srvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-    hr = device->CreateDescriptorHeap(&srvHeapDesc, IID_PPV_ARGS(&srvHeap));
+    hr = renderState.device->CreateDescriptorHeap(&srvHeapDesc, IID_PPV_ARGS(&renderState.srvHeap));
     if (FAILED(hr))
     {
         errhr("CreateDescriptorHeap failed (srvHeap)", hr);
         return 1;
     }
 
-    UINT cbvSrvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
-    UINT rtvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    UINT cbvSrvDescriptorSize = renderState.device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+    UINT rtvDescriptorSize = renderState.device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 
     // create frame resources
-    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandleSetup(rtvHeap->GetCPUDescriptorHandleForHeapStart());
-    ID3D12CommandAllocator *commandAllocators[frameCount] = {};
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandleSetup(renderState.rtvHeap->GetCPUDescriptorHandleForHeapStart());
+    
 
-    ID3D12Resource *renderTargets[frameCount] = {};
+    ID3D12Resource *renderTargets[renderState.frameCount] = {};
     // rtv for each buffer (double or triple buffering)
-    for (UINT n = 0; n < frameCount; n++)
+    for (UINT n = 0; n < renderState.frameCount; n++)
     {
-        hr = swapChain->GetBuffer(n, IID_PPV_ARGS(&renderTargets[n]));
+        hr = renderState.swapChain->GetBuffer(n, IID_PPV_ARGS(&renderTargets[n]));
         if (FAILED(hr))
         {
             errhr("GetBuffer failed", hr);
             return 1;
         }
-        device->CreateRenderTargetView(renderTargets[n], nullptr, rtvHandleSetup);
+        renderState.device->CreateRenderTargetView(renderTargets[n], nullptr, rtvHandleSetup);
         rtvHandleSetup.Offset(1, rtvDescriptorSize);
 
-        hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocators[n]));
+        hr = renderState.device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&renderState.commandAllocators[n]));
         if (FAILED(hr))
         {
             errhr("CreateCommandAllocator failed", hr);
@@ -217,7 +220,7 @@ int main(void)
     }
 
     ID3D12CommandAllocator *bundleAllocator = nullptr;
-    hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_BUNDLE, IID_PPV_ARGS(&bundleAllocator));
+    hr = renderState.device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_BUNDLE, IID_PPV_ARGS(&bundleAllocator));
     if (FAILED(hr))
     {
         errhr("CreateCommandAllocator failed", hr);
@@ -227,7 +230,7 @@ int main(void)
     // load assets
     D3D12_FEATURE_DATA_ROOT_SIGNATURE featureData = {};
     featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
-    hr = device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &featureData, sizeof(featureData));
+    hr = renderState.device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &featureData, sizeof(featureData));
     if (FAILED(hr))
     {
         featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
@@ -269,7 +272,7 @@ int main(void)
         return 1;
     }
     ID3D12RootSignature *rootSignature = nullptr;
-    hr = device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+    hr = renderState.device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
     if (FAILED(hr))
     {
         errhr("CreateRootSignature failed", hr);
@@ -319,7 +322,7 @@ int main(void)
     psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
     psoDesc.SampleDesc.Count = 1;
     ID3D12PipelineState *pipelineState = nullptr;
-    hr = device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&pipelineState));
+    hr = renderState.device->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&pipelineState));
     if (FAILED(hr))
     {
         errhr("CreateGraphicsPipelineState failed", hr);
@@ -327,7 +330,7 @@ int main(void)
     }
 
     ID3D12GraphicsCommandList *commandList = nullptr;
-    hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocators[frameIndex], nullptr, IID_PPV_ARGS(&commandList));
+    hr = renderState.device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, renderState.commandAllocators[frameIndex], nullptr, IID_PPV_ARGS(&commandList));
     if (FAILED(hr))
     {
         errhr("CreateCommandList failed", hr);
@@ -366,7 +369,7 @@ int main(void)
 
     CD3DX12_HEAP_PROPERTIES heapPropsUpload(D3D12_HEAP_TYPE_UPLOAD);
     CD3DX12_RESOURCE_DESC constantBufferDesc = CD3DX12_RESOURCE_DESC::Buffer(constantBufferSize);
-    hr = device->CreateCommittedResource(
+    hr = renderState.device->CreateCommittedResource(
         &heapPropsUpload,
         D3D12_HEAP_FLAG_NONE,
         &constantBufferDesc,
@@ -382,7 +385,7 @@ int main(void)
     D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc = {};
     cbvDesc.BufferLocation = constantBuffer->GetGPUVirtualAddress();
     cbvDesc.SizeInBytes = constantBufferSize;
-    device->CreateConstantBufferView(&cbvDesc, srvHeap->GetCPUDescriptorHandleForHeapStart());
+    renderState.device->CreateConstantBufferView(&cbvDesc, renderState.srvHeap->GetCPUDescriptorHandleForHeapStart());
 
     CD3DX12_RANGE readRangeCBV(0, 0);
     constantBuffer->Map(0, &readRangeCBV, reinterpret_cast<void **>(&CbvDataBegin));
@@ -395,7 +398,7 @@ int main(void)
     // code simplicity and because there are very few verts to actually transfer.
     ID3D12Resource *vertexBuffer = nullptr;
     CD3DX12_RESOURCE_DESC vertexBufferDesc = CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize);
-    hr = device->CreateCommittedResource(
+    hr = renderState.device->CreateCommittedResource(
         &heapPropsUpload,
         D3D12_HEAP_FLAG_NONE,
         &vertexBufferDesc,
@@ -419,7 +422,7 @@ int main(void)
 
     // CREATE BUNDLE
     ID3D12GraphicsCommandList *bundle = nullptr;
-    hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_BUNDLE, bundleAllocator, pipelineState, IID_PPV_ARGS(&bundle));
+    hr = renderState.device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_BUNDLE, bundleAllocator, pipelineState, IID_PPV_ARGS(&bundle));
     if (FAILED(hr))
     {
         errhr("CreateCommandList failed", hr);
@@ -458,7 +461,7 @@ int main(void)
 
     CD3DX12_HEAP_PROPERTIES heapPropsDefault(D3D12_HEAP_TYPE_DEFAULT);
     ID3D12Resource *texture = nullptr;
-    hr = device->CreateCommittedResource(
+    hr = renderState.device->CreateCommittedResource(
         &heapPropsDefault,
         D3D12_HEAP_FLAG_NONE,
         &textureDesc,
@@ -474,7 +477,7 @@ int main(void)
 
     // gpu upload buffer
     CD3DX12_RESOURCE_DESC uploadBufferDesc = CD3DX12_RESOURCE_DESC::Buffer(uploadBufferSize);
-    hr = device->CreateCommittedResource(
+    hr = renderState.device->CreateCommittedResource(
         &heapPropsUpload,
         D3D12_HEAP_FLAG_NONE,
         &uploadBufferDesc,
@@ -503,18 +506,18 @@ int main(void)
     srvDesc.Format = textureDesc.Format;
     srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
     srvDesc.Texture2D.MipLevels = 1;
-    D3D12_CPU_DESCRIPTOR_HANDLE srvHandleCPU = srvHeap->GetCPUDescriptorHandleForHeapStart();
+    D3D12_CPU_DESCRIPTOR_HANDLE srvHandleCPU = renderState.srvHeap->GetCPUDescriptorHandleForHeapStart();
     srvHandleCPU.ptr += cbvSrvDescriptorSize;
-    device->CreateShaderResourceView(texture, &srvDesc, srvHandleCPU);
+    renderState.device->CreateShaderResourceView(texture, &srvDesc, srvHandleCPU);
 
     commandList->Close();
     ID3D12CommandList *commandListsSetup[] = {commandList};
-    commandQueue->ExecuteCommandLists(_countof(commandListsSetup), commandListsSetup);
+    renderState.commandQueue->ExecuteCommandLists(_countof(commandListsSetup), commandListsSetup);
 
     // create synchronisation objects
     ID3D12Fence *fence = nullptr;
-    UINT64 fenceValues[frameCount] = {};
-    hr = device->CreateFence(fenceValues[frameIndex], D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
+    UINT64 fenceValues[renderState.frameCount] = {};
+    hr = renderState.device->CreateFence(fenceValues[frameIndex], D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
     fenceValues[frameIndex]++;
     if (FAILED(hr))
     {
@@ -575,13 +578,13 @@ int main(void)
 
         // render here
         // populate command list
-        hr = commandAllocators[frameIndex]->Reset();
+        hr = renderState.commandAllocators[frameIndex]->Reset();
         if (FAILED(hr))
         {
             errhr("Reset failed (command allocators)", hr);
             return 1;
         }
-        hr = commandList->Reset(commandAllocators[frameIndex], pipelineState);
+        hr = commandList->Reset(renderState.commandAllocators[frameIndex], pipelineState);
         if (FAILED(hr))
         {
             errhr("Reset failed (command list)", hr);
@@ -590,12 +593,12 @@ int main(void)
 
         commandList->SetGraphicsRootSignature(rootSignature);
 
-        ID3D12DescriptorHeap *heaps[] = {srvHeap};
+        ID3D12DescriptorHeap *heaps[] = {renderState.srvHeap};
         commandList->SetDescriptorHeaps(_countof(heaps), heaps);
 
-        commandList->SetGraphicsRootDescriptorTable(0, srvHeap->GetGPUDescriptorHandleForHeapStart()); // CBV
+        commandList->SetGraphicsRootDescriptorTable(0, renderState.srvHeap->GetGPUDescriptorHandleForHeapStart()); // CBV
 
-        D3D12_GPU_DESCRIPTOR_HANDLE srvHandleGPU = srvHeap->GetGPUDescriptorHandleForHeapStart();
+        D3D12_GPU_DESCRIPTOR_HANDLE srvHandleGPU = renderState.srvHeap->GetGPUDescriptorHandleForHeapStart();
         srvHandleGPU.ptr += cbvSrvDescriptorSize;
         commandList->SetGraphicsRootDescriptorTable(1, srvHandleGPU);
         commandList->RSSetViewports(1, &viewport);
@@ -604,7 +607,7 @@ int main(void)
         CD3DX12_RESOURCE_BARRIER commandListResourceBarrierTransitionRenderTarget = CD3DX12_RESOURCE_BARRIER::Transition(renderTargets[frameIndex], D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
         commandList->ResourceBarrier(1, &commandListResourceBarrierTransitionRenderTarget);
 
-        CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandlePerFrame(rtvHeap->GetCPUDescriptorHandleForHeapStart(), (INT)frameIndex, rtvDescriptorSize);
+        CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandlePerFrame(renderState.rtvHeap->GetCPUDescriptorHandleForHeapStart(), (INT)frameIndex, rtvDescriptorSize);
         commandList->OMSetRenderTargets(1, &rtvHandlePerFrame, FALSE, nullptr);
 
         // commands
@@ -630,49 +633,24 @@ int main(void)
 
         // execute command list
         ID3D12CommandList *commandListsPerFrame[] = {commandList};
-        commandQueue->ExecuteCommandLists(_countof(commandListsPerFrame), commandListsPerFrame);
+        renderState.commandQueue->ExecuteCommandLists(_countof(commandListsPerFrame), commandListsPerFrame);
 
-        hr = swapChain->Present(1, 0);
+        hr = renderState.swapChain->Present(1, 0);
         if (FAILED(hr))
         {
             errhr("Present failed", hr);
             return 1;
         }
 
-        // wait for prev frame (d3d11 style)
-        //  TODO: switch to more d3d12 style frame buffering (no waiting)
-        // const UINT64 localFenceValue = fenceValue; // DO NOT ACCIDENTLY CHANGE THE FENCE VALUE HERE, it needs to be the same thats why we are creating a const copy
-        // hr = commandQueue->Signal(fence, localFenceValue);
-        // if (FAILED(hr))
-        // {
-        //     errhr("Signal failed (command queue)", hr);
-        //     return 1;
-        // }
-        // fenceValue++; // now we chan change it this is deliberate
-
-        // if (fence->GetCompletedValue() < localFenceValue)
-        // {
-        //     hr = fence->SetEventOnCompletion(localFenceValue, fenceEvent);
-        //     if (FAILED(hr))
-        //     {
-        //         errhr("SetEventOnCompletion failed", hr);
-        //         return 1;
-        //     }
-        //     WaitForSingleObject(fenceEvent, INFINITE);
-        // }
-
-        // frameIndex = swapChain->GetCurrentBackBufferIndex();
-        // end of waiting
-
         // move to next frame
         const UINT64 currentFenceValue = fenceValues[frameIndex];
-        hr = commandQueue->Signal(fence, currentFenceValue);
+        hr = renderState.commandQueue->Signal(fence, currentFenceValue);
         if (FAILED(hr))
         {
             errhr("Signal failed", hr);
             return 1;
         }
-        frameIndex = swapChain->GetCurrentBackBufferIndex();
+        frameIndex = renderState.swapChain->GetCurrentBackBufferIndex();
 
         if (fence->GetCompletedValue() < fenceValues[frameIndex])
         {
@@ -713,29 +691,29 @@ int main(void)
         pipelineState->Release();
     if (rootSignature)
         rootSignature->Release();
-    if (srvHeap)
-        srvHeap->Release();
-    if (rtvHeap)
-        rtvHeap->Release();
-    for (UINT i = 0; i < frameCount; ++i)
+    if (renderState.srvHeap)
+        renderState.srvHeap->Release();
+    if (renderState.rtvHeap)
+        renderState.rtvHeap->Release();
+    for (UINT i = 0; i < renderState.frameCount; ++i)
     {
         if (renderTargets[i])
             renderTargets[i]->Release();
-        if (commandAllocators[i])
-            commandAllocators[i]->Release();
+        if (renderState.commandAllocators[i])
+            renderState.commandAllocators[i]->Release();
     }
-    if (swapChain)
-        swapChain->Release();
-    if (commandQueue)
-        commandQueue->Release();
+    if (renderState.swapChain)
+        renderState.swapChain->Release();
+    if (renderState.commandQueue)
+        renderState.commandQueue->Release();
     if (bundleAllocator)
         bundleAllocator->Release();
-    if (device)
-        device->Release();
-    if (hardwareAdapter)
-        hardwareAdapter->Release();
-    if (factory)
-        factory->Release();
+    if (renderState.device)
+        renderState.device->Release();
+    if (renderState.hardwareAdapter)
+        renderState.hardwareAdapter->Release();
+    if (renderState.factory)
+        renderState.factory->Release();
     if (vertexShader)
         vertexShader->Release();
     if (pixelShader)
